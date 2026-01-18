8dd4aa8d40fdb6b728f5d3ebd1f76f03c0b2b5e2
zengge99 <83830807+zengge99@users.noreply.github.com>
2026-01-18 10:16:36 +0800

    ok

--- release/getalist.sh
+++ release/getalist.sh
@@ -39,7 +39,7 @@ killall webdav_linux_$arch >/dev/null 2>&1
 ./movie_extractor_linux_$arch --srv 8889 >/dev/null 2>&1 &
 {
 	sleep 60
-	./webdav_linux_$arch --file "*.txt" --url "$server" --user guest 
+	./webdav_linux_$arch --file "*.txt" --url "$server" --user "$user" --password "$password" --port "$port" --obfuscate
 }&
 
 for task in "${tasks[@]}"; do
