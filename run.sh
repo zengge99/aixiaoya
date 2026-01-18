988ff2b3e707b8641327f2e7f86316d7fa186eb2
zengge99 <83830807+zengge99@users.noreply.github.com>
2026-01-18 11:31:06 +0800

    ok

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # --- 配置区 ---
-webdav_server=http://113.65.22.166:5678
+webdav_server=http://113.5.22.166:5678
 webdav_user="guest"
 webdav_password="guest_Api789"
 webdav_port=64000
 
-index_server=http://113.65.22.166:5678
+index_server=http://113.5.22.166:5678
 index_user="guest"
 index_password="guest_Api789"
 
