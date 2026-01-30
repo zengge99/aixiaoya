62a768fbd8d347a90b705dd7b7d859ff40ba9d16
zengge99 <83830807+zengge99@users.noreply.github.com>
2026-01-30 16:01:18 +0800

    ok

--- test/1.py
+++ test/1.py
@@ -2,7 +2,7 @@ from transformers import pipeline
 
 classifier = pipeline("zero-shot-classification")
 result = classifier(
-"九龙城寨之围城 蓝光原盘[国粤双语] [中英双字 国配中字 官译中字]",
+"九龙城寨之围城",
 candidate_labels=["movie", "education", "politics", "business"],
 )
 print(result)
