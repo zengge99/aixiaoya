0af43537591a439834ed844c00de5655e7d3a80a
zengge99 <83830807+zengge99@users.noreply.github.com>
2026-01-30 10:02:54 +0800

    ok

---
+++ test/1.py
@@ -0,0 +1,9 @@
+from transformers import pipeline
+
+classifier = pipeline("sentiment-analysis")
+result = classifier("I've been waiting for a HuggingFace course my whole life.")
+print(result)
+results = classifier(
+  ["I've been waiting for a HuggingFace course my whole life.", "I hate this so much!"]
+)
+print(results)
