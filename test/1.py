b79880606ab292c813e1f9e35fd18fbf67dbac0f
zengge99 <83830807+zengge99@users.noreply.github.com>
2026-01-30 16:18:04 +0800

    ok

--- test/1.py
+++ test/1.py
@@ -1,9 +1,5 @@
 from transformers import pipeline
 
-generator = pipeline("text-generation")
-results = generator(
-    "[CLS]我是",
-    num_return_sequences=2,
-    max_length=50
-) 
+ner = pipeline("ner", grouped_entities=True)
+results = ner("/我的115分享/电影/原盘/合集/06 高码率视频(电影为主，少量纪录片剧集演唱会)MKV REMUX格式为主，多为单片20G以上，初整理，不重，多版本除外) 437.92T 15937个文件(若干外挂字幕)/00画质控蓝光原盘REMUX 53.45T 2694/3. 电影-蓝光原盘REMUX/九龙城寨之围城 蓝光原盘REMUX [国粤双语] [中英双字 国配中字 官译中字]/Twilight.Of.The.Warriors.Walled.In.2024.1080p.BluRay.REMUX.AVC.DTS-HD.MA.TrueHD.7.1.Atmos.mkv")
 print(results)
