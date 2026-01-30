a73e03ae7fd27814a2152301721272fbd698e378
zengge99 <83830807+zengge99@users.noreply.github.com>
2026-01-30 16:15:25 +0800

    ok

--- test/1.py
+++ test/1.py
@@ -1,10 +1,8 @@
 from transformers import pipeline
 
 generator = pipeline("text-generation")
-results = generator("我是")
-print(results)
 results = generator(
-    "我是",
+    "[CLS]我是",
     num_return_sequences=2,
     max_length=50
 ) 
