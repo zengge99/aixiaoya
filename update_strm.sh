6fddd04b43c98f6f6de3004f0a891404c19cd7b2
zengge99 <83830807+zengge99@users.noreply.github.com>
2026-03-05 20:01:11 +0800

    ok

--- update_strm.sh
+++ update_strm.sh
@@ -54,7 +54,7 @@ while true; do
     echo "-------------------------------------------"
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] 开始更新任务..."
 
-    git pull 2>&1 >/dev/null
+    git pull 2>&1 >/dev/null 
     rm -f "$STRM_FILE" 2>&1 >/dev/null
 
     # 运行 Python 爬虫 (后台运行并等待，方便捕捉中断)
