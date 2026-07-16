d49a559106e148e5d6dddde21df1313cdb5a496e
zengge99 <83830807+zengge99@users.noreply.github.com>
2026-07-16 15:32:38 +0800

    Update strm_crawler.py

--- strm_crawler.py
+++ strm_crawler.py
@@ -105,7 +105,7 @@ def find_tv_root_context(strm_url):
         nfo_url = urljoin(search_url, "tvshow.nfo")
         data = parse_nfo_data(nfo_url)
         
-        if data and data.get("tmdbid"):
+        if data:
             info = {
                 "has_tvshow": True, 
                 "tmdbid": data.get("tmdbid"), 
